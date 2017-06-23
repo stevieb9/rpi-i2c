c6159944fbafa2d18132975bf9a917396395925a
Steve Bertrand <steveb@cpan.org>
2017-06-23 15:50:10 -0600

    updated Arduino sketch

--- examples/arduino.ino
+++ examples/arduino.ino
@@ -4,44 +4,122 @@
 #define DAC_0 A0
 #define DAC_1 A1
 
+#define READ        0
+#define READ_BYTE   5
+#define READ_BLOCK  10
+
+#define WRITE       25
+#define WRITE_BYTE  30
+#define WRITE_BLOCK 35
+
+#define READ_A0     80
+#define READ_A1     81
+
+int8_t reg;
+
 void send_data (){
-  Wire.write(analogRead(DAC_0));
+
+    switch (reg) {
+
+        case READ: {
+            Serial.println("read()");
+            Wire.write(reg);
+            break;
+        }
+        case READ_BYTE: {
+            Serial.println("read_byte()");
+            Wire.write(reg);
+            break;
+        }
+        case READ_BLOCK: {
+            Serial.println("read_block()");
+            int value = 1023;
+            uint8_t buf[2];
+
+            // reverse endian so we're big on the way out, and separate the
+            // 16-bit word
+
+            buf[1] = value & 0xFF;
+            buf[0] = (value & 0xFF00) >> 8;
+
+            Wire.write(buf, 2);
+            break;
+        }
+        case READ_A0: {
+            Serial.println("Analog 0");
+            read_analog(A0);
+            break;
+        }
+        case READ_A1: {
+            Serial.println("Analog 0");
+            read_analog(A0);
+            break;
+        }
+    }
+}
+
+int read_analog (int pin){
+    int val = analogRead(pin);
+    Serial.println(pin);
+
+    uint8_t buf[2];
+
+    // reverse endian so we're big endian going out
+
+    buf[1] = val & 0xFF;
+    buf[0] = (val & 0xFF00) >> 8;
+
+    Serial.print("0: ");
+    Serial.println(buf[0]);
+    Serial.print("1: ");
+    Serial.println(buf[1]);
+
+    Serial.println(val);
+
+    Wire.write(buf, 2);
 }
 
 void receive_data (int num_bytes){
-  Serial.print("bytes in: ");
-  Serial.println(num_bytes);
 
-  int16_t data;
-  
-  while(Wire.available()){
-    if (num_bytes > 1){
-      // throw away register byte if we received one
-      int8_t reg = Wire.read();
-      Serial.print("register addr: ");
-      Serial.println(reg);
+    int16_t data = 0;
+
+    while(Wire.available()){
+        // save the register value for use later
+        reg = Wire.read();
+
+        if (reg == WRITE){
+            Serial.println("write()");
+            data = reg;
+        }
+        if (reg == WRITE_BYTE){
+            Serial.println("write_byte()");
+            data = Wire.read();
+        }
+        if (reg == WRITE_BLOCK){
+            byte buf[4];
+
+            for (byte i=0; i<4; i++){
+                buf[i] = Wire.read();
+                data += buf[i];
+                Serial.print(i);
+                Serial.print(": ");
+                Serial.println(buf[i]);
+            }
+        }
+
+        Serial.print("data: ");
+        Serial.println(data);
     }
-    
-    if (num_bytes > 2){
-      data = Wire.read();
-      data += Wire.read() << 8;
-    }
-    else {
-      data = Wire.read();
-    }
-    
-    Serial.print("data: ");
-    Serial.println(data);
-  }
-  Serial.println("\n");
+    Serial.println("\n");
 }
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin(SLAVE_ADDR);
-  Wire.onReceive(receive_data);
-  Wire.onRequest(send_data);
+    Serial.begin(9600);
+    Wire.begin(SLAVE_ADDR);
+    Wire.onReceive(receive_data);
+    Wire.onRequest(send_data);
 }
 
 void loop() {
+    delay(1000);
 }
